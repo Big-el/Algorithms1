e85a799373743c22b10ecccb4a81d087bc33c806
ilaya oniko <ilayaoniko@gmail.com>
2025-02-19 15:00:58 +0100

    countingwords.js

--- countingwords.js
+++ countingwords.js
@@ -16,7 +16,7 @@ console.log(getLength('hello world'))
 //
 
 
-function getvowels(sentence){
+function getvowels(sentence){ 
     let count = 0;
     for(let i=0;i<sentence.length; i++){
         if(sentence[i]=='a'||sentence[i]=='A'||sentence[i]=='e'||sentence[i]=='E'||sentence[i]=='i'||sentence[i]=='I'||sentence[i]=='o'||sentence['O']||sentence[i]=='I'||sentence[i]=='u'||sentence[i]=='U'){
